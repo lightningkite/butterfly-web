150ae7cff4f804af6380cb50ec9daabf6b80ceea
Joseph Ivie <joseph@lightningkite.com>
2020-09-24 20:46:34 -0600

    Major renaming and cleaning

--- dist/observables/ObservableProperty.shared.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//! Declares com.lightningkite.butterfly.observables.ObservableProperty
-class ObservableProperty {
-    constructor() {
-    }
-}
-exports.ObservableProperty = ObservableProperty;
-//# sourceMappingURL=ObservableProperty.shared.js.map
