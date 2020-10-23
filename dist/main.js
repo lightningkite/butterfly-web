504f41ad352800c1759138751a7eac88d7eef245
Joseph Ivie <joseph@lightningkite.com>
2020-10-23 13:15:15 -0600

    Various updates for iOS and more

--- dist/main.js
+++ dist/main.js
@@ -3,12 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ViewDependency_1 = require("./views/ViewDependency");
 const HttpClient_1 = require("./net/HttpClient");
 const rxjs_1 = require("rxjs");
+const Language_1 = require("./kotlin/Language");
+const kotlin_text_1 = require("./kotlin/kotlin.text");
 function main(rootVg) {
     HttpClient_1.HttpClient.INSTANCE.ioScheduler = rxjs_1.asyncScheduler;
     HttpClient_1.HttpClient.INSTANCE.responseScheduler = rxjs_1.asyncScheduler;
     const view = rootVg.generate(window);
     document.body.appendChild(view);
     ViewDependency_1.listenForDialogs();
+    let r = Language_1.tryCastInterface(rootVg, "ComLightningkiteButterflyViewsEntryPoint");
+    if (r) {
+        let params = new Map();
+        if (window.location.search) {
+            for (const part of window.location.search.substring(1, window.location.search.length).split("&")) {
+                if (part.indexOf("=") != -1) {
+                    params.set(kotlin_text_1.xStringSubstringBefore(part, "="), kotlin_text_1.xStringSubstringAfter(part, "="));
+                }
+            }
+        }
+        r.handleDeepLink(window.location.protocol, window.location.host, window.location.pathname.substring(1, window.location.pathname.length), params);
+    }
 }
 exports.main = main;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import {ViewGenerator} from "./views/ViewGenerator";
 import {listenForDialogs} from "./views/ViewDependency";
 import {HttpClient} from "./net/HttpClient";
 import {asyncScheduler} from "rxjs";
+import {checkIsInterface, tryCastInterface} from "./kotlin/Language";
+import {root} from "rxjs/internal-compatibility";
+import {EntryPoint} from "./views/EntryPoint";
+import {xStringSubstringAfter, xStringSubstringBefore} from "./kotlin/kotlin.text";
 
 export function main(rootVg: ViewGenerator){
     HttpClient.INSTANCE.ioScheduler = asyncScheduler
@@ -9,4 +13,22 @@ export function main(rootVg: ViewGenerator){
     const view = rootVg.generate(window);
     document.body.appendChild(view);
     listenForDialogs();
+
+    let r = tryCastInterface<EntryPoint>(rootVg, "ComLightningkiteButterflyViewsEntryPoint")
+    if(r){
+        let params = new Map<string, string>()
+        if(window.location.search){
+            for(const part of window.location.search.substring(1, window.location.search.length).split("&")) {
+                if(part.indexOf("=") != -1) {
+                    params.set(xStringSubstringBefore(part, "="), xStringSubstringAfter(part, "="))
+                }
+            }
+        }
+        r.handleDeepLink(
+            window.location.protocol,
+            window.location.host,
+            window.location.pathname.substring(1, window.location.pathname.length),
+            params
+        )
+    }
 }
